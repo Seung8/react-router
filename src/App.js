0ce602b8c0ca0125e58697f5c8f92846c7675efd
Seungyun Lee <lsy0914@naver.com>
2020-10-11 16:39:00 +0900

    - Switch 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import About from "./About";
 import HistorySample from "./HistorySample";
 import Home from "./Home";
@@ -23,10 +23,24 @@ function App() {
         </li>
         <hr />
       </ul>
-      <Route path="/" component={Home} exact />
-      <Route path="/about" component={About} />
-      <Route path="/profiles" component={Profiles} />
-      <Route path="/history" component={HistorySample} />
+      {/*
+        Switch는 위에서부터  순차적으로 Route 컴포넌트를 읽으며 아무것도 찾지 못하면
+        가장 아래에 위치한(path, component 속성 없이 render 속성만 있는) Route 컴포넌트를 노출한다. 
+      */}
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>페이지를 찾을 수 없습니다...</h2>
+              <p>{location.pathname} 페이지는 존재하지 않습니다.</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   );
 }
